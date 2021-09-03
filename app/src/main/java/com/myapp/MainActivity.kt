cd7a52b07d9dab8e22eb1a84f33afaa4cd5bf7a1
agamirzaev <kabiragamirzaev@gmail.com>
2021-09-03 12:04:02 +0300

    qwerttzxv

--- app/src/main/java/com/myapp/MainActivity.kt
+++ app/src/main/java/com/myapp/MainActivity.kt
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var a:Int = 0
+    var a: Int = 1
+    var b: Int = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val c: Int = a + b
+        print("$c")
     }
 }
